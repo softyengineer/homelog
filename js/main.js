06f2657dd66170e24996b5177b06d53f6ed065bd
tom <tom.uwe@gmail.com>
2019-04-23 20:51:04 +0100

    More pics, main.js

--- js/main.js
+++ js/main.js
@@ -2,4 +2,23 @@ function fillGeneric() {
   textbox = document.getElementById("js_title");
   txt = document.createTextNode("A DIY renovation logbook");
   textbox.appendChild(txt);
+  fillFooter1();
+  fillFooter2();
+}
+function fillFooter1() {
+  textbox = document.getElementById("js_footer");
+  txt = document.createTextNode("Buy Bitcoin using BISQ");
+  textbox.appendChild(txt);
+}
+function fillFooter2() {
+  textbox = document.getElementById("js_footer2");
+  txt = document.createTextNode("Buy Bitcoin from LocalBitcoins");
+  textbox.appendChild(txt);
+}
+function arrangePhotos() {
+  var elements = document.getElementsByClassName("column");
+  var i;
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "50%";
+  }
 }
