a1005c23f0ee82ee2ba09b978e10868cc9b0ec31
tom <tom.uwe@gmail.com>
2019-04-28 22:29:57 +0100

    Smarter and dumber js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
+document.body.onload = insertHtml();
+document.body.onload = fillGeneric();
+document.body.onload = fillFooter1();
+document.body.onload = fillFooter2();
+
 function fillGeneric() {
   textbox = document.getElementById("js_title");
   txt = document.createTextNode("A DIY renovation logbook");
   textbox.appendChild(txt);
-  fillFooter1();
-  fillFooter2();
 }
 function fillFooter1() {
   textbox = document.getElementById("js_footer");
@@ -25,4 +28,43 @@ function arrangePhotos(inputint) {
       elements[iOut + i * elements.length].style.flex = 100 / cols + "%";
     }
   }
+}
+function insertHtml() {
+  //get page
+  var path = window.location.pathname;
+  var page = path.split("/").pop();
+  switch (page) {
+    case index:
+      var index = document.createTextNode("active");
+      break;
+    default:
+  }
+  var first = document.createTextNode(
+    '<ul class="nav">\n' + '<li class="nav"><a class="'
+  );
+  var second = document.createTextNode(
+    '" href="./index.html">Home</a></li>\n' + '<li class="nav"><a class="'
+  );
+  var third = document.createTextNode(
+    '" href="./photos.html">Photos</a></li>\n' + '<li class="nav"><a class="'
+  );
+  var fourth = document.createTextNode(
+    '" href="./diary.html">Diary</a></li>\n' +
+      '<li style="float:right">\n' +
+      '<a id="js_title" class="blank" href="./info.html"></a></li></ul>\n' +
+      "</li>\n" +
+      "</ul>\n"
+  );
+
+  var newDiv = document.createElement("div");
+
+  var nav = document.createTextNode("");
+  nav += first;
+  nav += second;
+  nav += third;
+  nav += fourth;
+  newDiv.appendChild(nav);
+
+  // document.getElementById("top").appendChild(nav);
+  document.getElementById("top").innerHTML = nav;
 }
